4be5cbeefd45015843b18a5003d4e277d25e7b87
Unionindesign <stevemarshall89@yahoo.com>
2019-09-03 13:05:12 -0600

    Removed local data. Unable to fetch data using axios or papaparse inside of stackblitz, added data to state to pass down as prop

--- map.js
+++ map.js
@@ -11,13 +11,15 @@ const TOKEN = "pk.eyJ1Ijoic3RlcGhlbi1tYXJzaGFsbCIsImEiOiJjanB2aWxlNWMwMHV3NDJraj
 class WorldMap extends Component {
   constructor() {
     super();
+    //ref to map markers
+    this.MapMarkers=[]
     this.state = {
       viewport: {
 				width: window.innerWidth,
 				height: window.innerHeight * 0.8,
-				latitude: 30,
-				longitude: 1,
-				zoom: 1.5,
+				latitude: 37.785164,
+				longitude: -100,
+				zoom: 1,
 				pitch: 0
 			},
     };
@@ -39,6 +41,28 @@ class WorldMap extends Component {
 		// 	this.setState({ viewport });
 		// }
 	};
+  componentDidMount(){
+    //console.log("Props Check: ", this.props.cities)
+  }
+  	markersizer = pop => {
+      //let pop = pop.replace(/,/g, '')
+		if (pop < 600000) {
+			return 2;
+		} else if (pop >= 600000 && pop <= 750000) {
+			return 3;
+		} else if (pop >= 750000 && pop <= 1000000) {
+			return 4;
+		} else if (pop >= 2000000 && pop <= 3500000) {
+			return 5;
+		} else if (pop >= 3500000 && pop <= 5000000) {
+			return 6;
+		} else if (pop >= 5000000 && pop <= 65000000) {
+			return 7;
+    } else if (pop >= 80000000) {
+      return 8;
+    }
+
+	};
 
   render() {
     return (
@@ -48,7 +72,31 @@ class WorldMap extends Component {
 					{...this.state.viewport}
 					viewport={this.state.viewport}
 					mapStyle="mapbox://styles/mapbox/dark-v9"
-					onViewportChange={this._onViewportChange}/>
+					onViewportChange={this._onViewportChange}>
+            {this.props.cities.map((city, i) => (
+						<Marker
+							key={i}
+							latitude={city.latitude}
+							longitude={city.longitude}
+							offsetLeft={-20}
+							offsetTop={-10}>
+							<svg>
+									<circle
+										cx={12}
+										cy={12}
+                    //manipulating string data to show larger radius marker based on population
+										r={this.markersizer(parseInt(city.population)*1000)}
+										ref={e => (this.MapMarkers[i] = e)}
+										style={{
+											fill: "red",
+											stroke: "#FFF",
+											strokeWidth: 2
+										}}
+									/>
+								</svg>
+						</Marker>
+					))}
+          </MapGL>
       </div>
     );
   }
